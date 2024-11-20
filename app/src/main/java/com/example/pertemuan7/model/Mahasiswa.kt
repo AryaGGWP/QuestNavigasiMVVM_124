7e105e99b5cabd978098916c61f6b026e79e1574
AryaGGWP <arya.lampung.2016@gmail.com>
2024-11-20 23:09:20 +0700

    5th commit: menambahkan val data class Mahasiswa

--- app/src/main/java/com/example/pertemuan7/model/Mahasiswa.kt
+++ app/src/main/java/com/example/pertemuan7/model/Mahasiswa.kt
@@ -1,7 +1,12 @@
 package com.example.pertemuan7.model
 
+import android.provider.ContactsContract.CommonDataKinds.Email
+
 data class Mahasiswa (
     val nama: String = "",
+    val nim: String = "",
+    val email: String = "",
     val gender: String = "",
-    val alamat: String = ""
+    val alamat: String = "",
+    val noHp: String = ""
 )
